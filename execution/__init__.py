04efe18e0a50eeca36b3404a487c96625999efbf
officialoroe-crypto <officialoroe@gmail.com>
2026-09-17 14:46:11 -0700

    Expose execution resource policy components

--- execution/__init__.py
+++ execution/__init__.py
@@ -1,3 +1,17 @@
 from execution.engine import ExecutionEngine
+from execution.policy import TaskClass, classify_task, local_execution_allowed
+from execution.resource import ResourceBand, ResourceGuard, ResourceSnapshot, resource_guard
 
 execution_engine = ExecutionEngine()
+
+__all__ = [
+    "ExecutionEngine",
+    "ResourceBand",
+    "ResourceGuard",
+    "ResourceSnapshot",
+    "TaskClass",
+    "classify_task",
+    "execution_engine",
+    "local_execution_allowed",
+    "resource_guard",
+]
