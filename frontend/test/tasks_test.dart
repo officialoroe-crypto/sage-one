7c1cf89934b00d1c0d91446f01e1b041fd1a54fe
officialoroe-crypto <officialoroe@gmail.com>
2026-09-18 05:43:59 -0700

    fix: import Flutter Material in task UX test

--- frontend/test/tasks_test.dart
+++ frontend/test/tasks_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:sage_one/core/sage_api.dart';
