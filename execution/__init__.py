d969abf15099663bd2a781889510356ea1fbf52f
officialoroe-crypto <officialoroe@gmail.com>
2026-09-17 23:36:25 -0700

    Reconcile execution exports with main

--- execution/__init__.py
+++ execution/__init__.py
@@ -1,17 +1,26 @@
 from execution.engine import ExecutionEngine
+from execution.mode import ConnectivityState, ExecutionMode, ExecutionModeRouter, mode_router
 from execution.policy import TaskClass, classify_task, local_execution_allowed
 from execution.resource import ResourceBand, ResourceGuard, ResourceSnapshot, resource_guard
+from execution.scheduler import ExecutionScheduler, ScheduledTask, scheduler
 
 execution_engine = ExecutionEngine()
 
 __all__ = [
+    "ConnectivityState",
     "ExecutionEngine",
+    "ExecutionMode",
+    "ExecutionModeRouter",
+    "ExecutionScheduler",
     "ResourceBand",
     "ResourceGuard",
     "ResourceSnapshot",
+    "ScheduledTask",
     "TaskClass",
     "classify_task",
     "execution_engine",
     "local_execution_allowed",
+    "mode_router",
     "resource_guard",
+    "scheduler",
 ]
