450d76bbb14b3d3846fa0be23bff70cc76e1418f
officialoroe-crypto <officialoroe@gmail.com>
2026-09-18 03:27:09 -0700

    feat: add task cancellation to mobile API

--- frontend/lib/core/sage_api.dart
+++ frontend/lib/core/sage_api.dart
@@ -34,7 +34,7 @@ class SageApi {
     return items is List ? items : <dynamic>[];
   }
 
-  Future<Map<String, dynamic>> task(String taskId) async {
+  Future<Map<String, dynamic>> cancelTask(String taskId) async {\n    final response = await _client.post(Uri.parse('$baseUrl/tasks/$taskId/cancel'));\n    return _decode(response);\n  }\n\n  Future<Map<String, dynamic>> task(String taskId) async {
     final response = await _client.get(Uri.parse('$baseUrl/tasks/$taskId'));
     return _decode(response);
   }
