7ce9f66b123a51d0bb015dfcd2f6404e45344abe
SAGE - ONE <officialoroe@gmail.com>
2026-09-20 00:37:39 -0700

    Fix non-const Google web button configuration

--- frontend/lib/core/google_web_button_web.dart
+++ frontend/lib/core/google_web_button_web.dart
@@ -3,7 +3,7 @@ import 'package:google_sign_in_web/web_only.dart' as google_web;
 
 Widget buildGoogleWebButton() {
   return google_web.renderButton(
-    configuration: const google_web.GSIButtonConfiguration(
+    configuration: google_web.GSIButtonConfiguration(
       type: google_web.GSIButtonType.standard,
       theme: google_web.GSIButtonTheme.outline,
       size: google_web.GSIButtonSize.large,
